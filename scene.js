e55aa736db4b01f005d819f6ffe0d10f06b6763a
Mic75 <mat.benard@gmail.com>
2020-08-17 11:50:20 -0400

    Smooth rotatation with tweenjs

--- scene.js
+++ scene.js
@@ -1,9 +1,20 @@
 import * as THREE from './node_modules/three/build/three.module.js';
+import TWEEN from "./node_modules/@tweenjs/tween.js/dist/tween.esm.js";
 import {HALF_PI} from "./constants.js";
 
 var camera, scene, renderer;
 var geometry, material, mesh;
+let rotation = {
+  x: 0.0,
+  z: 0.0
+};
 
+let rotationTarget = {
+  x: 0.0,
+  z: 0.0
+};
+
+let tween = {update(){}};
 
 export function init() {
 
@@ -20,30 +31,27 @@ export function init() {
   mesh = new THREE.Mesh( geometry, material );
   scene.add( mesh );
 
+
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
-
 }
 
 export function updateMeshRotation(accelData){
-  const sensitivityMax = 0.2;
-  const rZ = -(accelData.x * HALF_PI) / 1.024;
-  const rX = -(accelData.y * HALF_PI) / 1.024;
-
-  const deltatRz = Math.abs(rZ - mesh.rotation.z);
-  const deltatRx = Math.abs(rX - mesh.rotation.x);
-
-  if (deltatRz > sensitivityMax) {
-    mesh.rotation.z = rZ;
-  }
-
-  if (deltatRx > sensitivityMax) {
-    mesh.rotation.x = rX;
-  }
+  rotationTarget.z =   -(accelData.x * HALF_PI) / 1.024;
+  rotationTarget.x = (accelData.y * HALF_PI) / 1.024;
+  tween = new TWEEN.Tween(rotation)
+    .to(rotationTarget, 200)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .onUpdate(() => {
+      mesh.rotation.z = rotation.z;
+      mesh.rotation.x = rotation.x;
+    })
+    .start();
 }
 
-export function animate() {
+export function animate(time) {
   requestAnimationFrame( animate );
   renderer.render( scene, camera );
+  tween.update(time);
 }
