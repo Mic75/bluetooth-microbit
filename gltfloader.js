56488ae1193d0a157da809245ac9b3747f33ee61
Mic75 <mat.benard@gmail.com>
2020-08-20 19:23:18 -0400

    Load from three form cwd in gltfloader

--- gltfloader.js
+++ gltfloader.js
@@ -61,7 +61,7 @@ import {
   Vector3,
   VectorKeyframeTrack,
   sRGBEncoding
-} from "./node_modules/three/build/three.module.js";
+} from "./three.module.js";
 
 var GLTFLoader = ( function () {
 
